eefe76dc9de38994d4db75e6da34924811758148
Nicholas Molyneaux <nicholas.molyneaux@epfl.ch>
2018-07-23 17:25:22 +0200

    fixed error in parameter exploration

--- src/main/scala/optimization/bruteforce/ParameterExploration.scala
+++ src/main/scala/optimization/bruteforce/ParameterExploration.scala
@@ -3,7 +3,7 @@ package optimization.bruteforce
 import com.typesafe.config.Config
 import hubmodel.DES.SFGraphSimulator
 import hubmodel.mgmt.ControlDevices
-import hubmodel.{runAndCollect}
+import hubmodel.runAndCollect
 import hubmodel.supply.graph.FlowGateFunctional
 import myscala.math.stats.ComputeStats
 
@@ -13,25 +13,15 @@ import scala.collection.parallel.ForkJoinTaskSupport
 
 class ParameterExploration(val referenceSimulator: SFGraphSimulator, config: Config) {
 
-  if (config.getInt("sim.nb_runs") < config.getInt("execution.threads")) {
-    println("WARNING ! NUMBER OF RUNS SMALLER THAN NUMBER OF THREADS TO USE ! NOT EFFICIENT")
-  }
-
-  def exploreFlowGateFunctionalFormLinear(constantBounds: (Double, Double, Int), linearBounds: (Double, Double, Int)): Iterable[(Double, Double, (Int, Double, Double, Double, Double, Double), (Int, Double, Double, Double, Double, Double))] = {
+  def exploreFlowGateFunctionalFormLinear(constantBounds: (Double, Double, Int), linearBounds: (Double, Double, Int)): Map[(Double, Double), ((Int, Double, Double, Double, Double, Double), (Int, Double, Double, Double, Double, Double))] = {
 
     val defaultParameters = referenceSimulator.getSetupArguments
 
     val constantRange: NumericRange[Double] = constantBounds._1 to constantBounds._2 by (constantBounds._2 - constantBounds._1) / constantBounds._3
     val linearRange: NumericRange[Double] = linearBounds._1 to linearBounds._2 by (linearBounds._2 - linearBounds._1) / linearBounds._3
 
-
-
-
-    for (i <- constantRange; j <- linearRange) yield {
-
-      println("Running simulation with linear flow gate function: flowrate = " + i + " + " + j + "*density")
-
-      val simulationCollection: collection.parallel.ParSeq[SFGraphSimulator] = collection.parallel.ParSeq.fill(config.getInt("sim.nb_runs"))({
+    val sims = (for (i <- constantRange; j <- linearRange) yield {
+      Vector.fill(config.getInt("sim.nb_runs"))({
 
         val newDevices: ControlDevices = new ControlDevices(
           defaultParameters._11.monitoredAreas.map(_.clone()),
@@ -41,7 +31,7 @@ class ParameterExploration(val referenceSimulator: SFGraphSimulator, config: Con
           defaultParameters._11.flowSeparators.map(_.clone())
         )
 
-        new SFGraphSimulator(
+        (i,j,new SFGraphSimulator(
           defaultParameters._1,
           defaultParameters._2,
           defaultParameters._3,
@@ -54,16 +44,15 @@ class ParameterExploration(val referenceSimulator: SFGraphSimulator, config: Con
           defaultParameters._10,
           newDevices
         )
-      }
-      )
-      simulationCollection.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(config.getInt("execution.threads")))
-      val simulationResults = simulationCollection.par.map(runAndCollect).seq.toVector
-      val ttStats = simulationResults.flatMap(r => r._1.map(_.travelTime.value)).stats
-      val densityStats = simulationResults.flatMap(r => r._2.values.flatMap(_.densityHistory.map(_._2))).stats
+        )
+      })
+    }).toVector.flatten.par
 
-
-      (i, j, ttStats, densityStats)
-    }
+      //println("Running simulation with linear flow gate function: flowrate = " + i + " + " + j + "*density")
+    sims.tasksupport = new ForkJoinTaskSupport(new java.util.concurrent.ForkJoinPool(config.getInt("execution.threads")))
+      val simulationResults = sims.map(sim => (sim._1, sim._2, runAndCollect(sim._3))).seq.groupBy(tup => (tup._1, tup._2))
+      simulationResults.map(tu => tu._1 -> (tu._2.flatMap(r => r._3._1.map(_.travelTime.value)).stats, tu._2.flatMap(r => r._3._2.values.flatMap(_.densityHistory.map(_._2))).stats))
+      //val densityStats = simulationResults.map(tu => (tu._1, tu._2.flatMap(r => r._3._2.values.flatMap(_.densityHistory.map(_._2))).stats))
 
   }
 
